cc680e7a3ac8d6866afb8083c63008cc70c362f6
MadsenMartin <martinf.madsen159@gmail.com>
2026-09-14 10:56:49 -0300

    Cambio seed para que coincida con valores de producción

--- Backend/iva/migrations/0002_seed.py
+++ Backend/iva/migrations/0002_seed.py
@@ -6,9 +6,9 @@ from django.db import migrations, models
 
 
 def seed_data(apps, schema_editor):
-        iva.models.Imputacion.objects.create(imputacion='Mov. entre cuentas', activo=True)
-        shared.models.Moneda.objects.create(nombre='BRL', activo=True)
-        shared.models.Moneda.objects.create(nombre='USD', activo=True)
+        iva.models.Imputacion.objects.get_or_create(imputacion='Mov. entre cuentas', activo=True)
+        shared.models.Moneda.objects.get_or_create(nombre='R$', activo=True)
+        shared.models.Moneda.objects.get_or_create(nombre='U$D', activo=True)
         #iva.models.Caja.objects.create(caja='Santander BRL', activo=True, moneda=shared.models.Moneda.objects.get(nombre='BRL'), codigo='BSRS')
         #iva.models.Caja.objects.create(caja='Chase USD', activo=True, moneda=shared.models.Moneda.objects.get(nombre='USD'), codigo='CHAS')
         #iva.models.Caja.objects.create(caja='Caja Santi R$', activo=True, moneda=shared.models.Moneda.objects.get(nombre='BRL'), codigo='CRSSS')
